abcd6f9ac6a5ac05c88ee368450b4c5d0409b01e
edithcalm <edithkaranja02@gmail.com>
2025-04-08 20:20:50 +0300

    changing the currency format from USD to KSH

--- src/components/CurrencyFormatter/CurrencyFormatter.js
+++ src/components/CurrencyFormatter/CurrencyFormatter.js
@@ -4,12 +4,12 @@ import { isNumeric } from '../../helpers/general';
 
 const CurrencyFormatter = ({
   amount,
-  currency = 'USD',
+  currency = 'KSH',
   appendZero = false,
   useDollar = false,
 }) => {
   let displayAmount =
-    (typeof amount !== 'number' && parseFloat(amount?.replace('$', ''))) ||
+    (typeof amount !== 'number' && parseFloat(amount?.replace('KSH', ''))) ||
     amount;
   /* Set language display */
   const languageCode =
@@ -23,7 +23,7 @@ const CurrencyFormatter = ({
     style: 'currency',
     currency,
   });
-  let symbol = '$';
+  let symbol = 'KSH';
   let formattedPrice = formatObject.format(displayAmount);
   if ('formatToParts' in formatObject) {
     const formattedPriceParts = formatObject.formatToParts(displayAmount);
